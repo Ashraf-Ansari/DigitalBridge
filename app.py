7a67b80794cad5bcc5d197088c06f2015a9bf67e
Ashraf <ashraf.ali@kuliza.com>
2022-08-04 01:16:51 +0530

    beidge api

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 sqlite_insert_blob_query = """INSERT INTO badges (name, description,badge,students) VALUES (?, ? ,?,?)"""
-
+image_basepath = "static/images/"
+allData = 'select * from badges'
 def delete_file(filepath):
 
     if os.path.isfile(filepath):
@@ -33,7 +34,7 @@ def index():
 @app.route('/')
 def upload():
     con = get_db_connection()
-    result = con.execute('select * from badges').fetchall()
+    result = con.execute(allData).fetchall()
     print("length",len(result))
     con.close()
     return render_template("file_upload_form.html")
@@ -48,6 +49,8 @@ def writeTofile(data, filename):
     with open(filename, 'wb') as file:
         file.write(data)
     print("Stored blob data into: ", filename, "\n")
+
+
 
 # def generate_file_name(name):
 #     gmt = time.gmtime()
@@ -71,16 +74,69 @@ def success():
         if file.filename=="":
             print("empty")
         else:
-            file_path = "images/"+file.filename
+            file_path = image_basepath+file.filename
             print(file, description, name, students,file_path)
             file.save(file_path)
-            file_data = convertToBinaryData(file.filename)
+            file_data = convertToBinaryData(file_path)
             con = get_db_connection()
             con.execute(sqlite_insert_blob_query,(name,description,file_data,students))
             con.commit()
             con.close()
-            delete_file(file_path)
+            # delete_file(file_path)
         return render_template("success.html")
+
+@app.route('/detection')
+def detection():
+    dir = os.walk(image_basepath)
+    print("dir",dir)
+    file_list = []
+
+    for path, subdirs, files in dir:
+        for file in files:
+            print(path,file)
+            temp = path + file
+            print(temp)
+            file_list.append(temp)
+    print("all files ",file_list)
+    return render_template('showpictures.html', filenames=file_list)
+
+@app.route('/showpictures')
+def showpictures():
+    con = get_db_connection()
+    result = con.execute(allData).fetchall()
+    print("length", len(result))
+    file_list = []
+    for i in result:
+        file_list.append(i[3])
+        print(i[0],i[3])
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    return render_template('showpictures.html', filenames=file_list)
+
+@app.route('/badge/verify', methods=['GET'])
+def search():
+    args = request.args
+    args = args.to_dict();
+    print(args)
+    name = args.get("name")
+    email = args.get("email")
+    con = get_db_connection()
+    result = con.execute(allData).fetchall()
+    print("length", len(result))
+    finalResult = []
+    for i in result:
+        if name==i[1]:
+            emails = str(i[4]).split(",")
+            for j in emails:
+                if j==email:
+                    finalResult.append(i)
+    print(finalResult)
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
